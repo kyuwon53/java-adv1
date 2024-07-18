2545553119d34f399a3aaf3c67fabee5c568a96a
kyuwon lee <lkw2v@naver.com>
2024-07-18 11:00:27 +0900

    스레드 생명 주기

---
+++ src/thread/control/ThreadStateMain.java
@@ -0,0 +1,40 @@
+package thread.control;
+
+import static util.MyLogger.log;
+
+public class ThreadStateMain {
+    public static void main(String[] args) throws InterruptedException {
+        Thread myThread = new Thread(new MyRunnable(), "myThread");
+        log("myThread..state1 = " + myThread.getState());   // NEW
+
+        log("myThread.start()");
+        myThread.start();
+
+        Thread.sleep(1000);
+        log("myThread.state3 = " + myThread.getState());    // TIMED_WAITING
+
+        Thread.sleep(4000);
+        log("myThread.state5 = " + myThread.getState());    // TERMINATED
+
+        log("end");
+    }
+
+    static class MyRunnable implements Runnable {
+        @Override
+        public void run() {
+            try {
+                log("start");
+                log("myThread.state2 = " + Thread.currentThread().getState());  // RUNNABLE
+
+                log("sleep start");
+                Thread.sleep(3000);
+                log("sleep end");
+
+                log("myThread.state4 = " + Thread.currentThread().getState());  // RUNNABLE
+                log("end");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
